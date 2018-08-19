0ec524f5ce60545d474a7b8a2d0d8daa7ce1ceaf
jnschrag <jnschrag@gmail.com>
2018-08-19 15:57:47 -0400

    Add full declaration text and ToC

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -13,4 +13,42 @@ function notificationBar() {
    	}
 }
 
-window.addEventListener('DOMContentLoaded', notificationBar)
+function htmlTableOfContents() {
+    const toc = document.getElementById("table-of-contents");
+	const headings = Array.from(document.body.querySelectorAll('.post-content h1, .post-content h2, .post-content h3, .post-content h4, .post-content h5, .post-content h6'));
+	let currentLevel = 0
+	let tocHTML = '<ul>'
+    headings.forEach(function (heading, index) {
+    	let headingLevel = heading.tagName.slice(-1)
+    	if ( index == 0 ) {
+    		currentLevel = headingLevel
+    	}
+
+    	let itemHTMLPrefix = ''
+    	let itemHTMLSuffix = ''
+    	if (headingLevel < currentLevel) {
+    		itemHTMLPrefix = '</ul></li>'
+    		itemHTMLSuffix = ''
+    	} else if (headingLevel > currentLevel ) {
+    		itemHTMLPrefix = '<ul>'
+    		itemHTMLSuffix = ''
+    	}
+    	currentLevel = heading.tagName.slice(-1)
+
+        let ref = "toc" + index;
+        if ( heading.hasAttribute( "id" ) ) 
+            ref = heading.getAttribute( "id" );
+        else
+            heading.setAttribute( "id", ref );
+
+        tocHTML += itemHTMLPrefix + '<li data-level="' + headingLevel + '"><a href="#' + ref + '">' + heading.textContent + '</a>' + itemHTMLSuffix
+
+    });
+    tocHTML += '</li></ul>'
+    toc.innerHTML = tocHTML
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+	notificationBar()
+	htmlTableOfContents()
+})
